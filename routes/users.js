a6a256969f89b9328afea354050e6a12aa734d92
Fırat Doğan <fratdogan@yandex.com>
2022-11-22 23:45:30 +0300

    fix: empty request error handling for /users

--- routes/users.js
+++ routes/users.js
@@ -21,32 +21,46 @@ router.post("/", async (req, res, next) => {
                 status: "error",
                 message: "The user name is already created.",
             });
+        } else if (error.code === "ERR_INVALID_ARG_TYPE") {
+            res.status(400).json({
+                status: "error",
+                message: "Request is invalid",
+            });
         }
     }
 });
 
 router.get("/", async (req, res, next) => {
-    if (await user.doesExist(req.body.username, req.body.password)) {
-        const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-        const jwt = await new jose.SignJWT({})
-            .setProtectedHeader({
-                alg: "HS256",
-            })
-            .setIssuer(req.headers.referer)
-            .setAudience(req.body.username)
-            .setIssuedAt()
-            .setExpirationTime("1d")
-            .sign(secret);
+    try {
+        if (await user.doesExist(req.body.username, req.body.password)) {
+            const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+            const jwt = await new jose.SignJWT({})
+                .setProtectedHeader({
+                    alg: "HS256",
+                })
+                .setIssuer(req.headers.referer)
+                .setAudience(req.body.username)
+                .setIssuedAt()
+                .setExpirationTime("1d")
+                .sign(secret);
 
-        res.status(200).json({
-            status: "success",
-            data: jwt,
-        });
-    } else {
-        res.status(404).json({
-            status: "error",
-            message: "User does not exist",
-        });
+            res.status(200).json({
+                status: "success",
+                data: jwt,
+            });
+        } else {
+            res.status(404).json({
+                status: "error",
+                message: "User does not exist",
+            });
+        }
+    } catch (error) {
+        if (error.code === "ERR_INVALID_ARG_TYPE") {
+            res.status(400).json({
+                status: "error",
+                message: "Request is invalid",
+            });
+        }
     }
 });
 
