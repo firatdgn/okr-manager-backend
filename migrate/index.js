993e9f166b107734a6bd1ca98e94fbc3e4378e17
Fırat Doğan <fratdogan@yandex.com>
2022-12-03 13:11:43 +0300

    fix: migrate.js is killing process after migration

--- migrate/index.js
+++ migrate/index.js
@@ -2,16 +2,23 @@ require("dotenv").config();
 
 const db = require("../core/db-connection");
 
-db.query(
-    `
+let users = new Promise((resolve, reject) => {
+    db.query(
+        `
 	CREATE TABLE IF NOT EXISTS users (
 		username varchar(255) NOT NULL UNIQUE,
 		password text NOT NULL
 	)
-`
-);
-db.query(
-    `
+`,
+        (err, res) => {
+            if (err) reject(err);
+            if (res) resolve(res);
+        }
+    );
+});
+let bhags = new Promise((resolve, reject) => {
+    db.query(
+        `
 	CREATE TABLE IF NOT EXISTS bhags (
 		id INT(255) UNSIGNED NOT NULL AUTO_INCREMENT,
 		content text NOT NULL,
@@ -19,10 +26,16 @@ db.query(
 		deleted_at TIMESTAMP NULL DEFAULT NULL,
 		PRIMARY KEY (id)
 	)
-`
-);
-db.query(
-    `
+`,
+        (err, res) => {
+            if (err) reject(err);
+            if (res) resolve(res);
+        }
+    );
+});
+let quarters = new Promise((resolve, reject) => {
+    db.query(
+        `
 	CREATE TABLE IF NOT EXISTS quarters (
 		id INT(255) UNSIGNED NOT NULL AUTO_INCREMENT,
 		started_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
@@ -32,10 +45,16 @@ db.query(
 		PRIMARY KEY (id),
 		FOREIGN KEY (bhag_id) REFERENCES bhags(id)
 	)
-`
-);
-db.query(
-    `
+`,
+        (err, res) => {
+            if (err) reject(err);
+            if (res) resolve(res);
+        }
+    );
+});
+let objectives = new Promise((resolve, reject) => {
+    db.query(
+        `
 	CREATE TABLE IF NOT EXISTS objectives (
 		id INT(255) UNSIGNED NOT NULL AUTO_INCREMENT,
 		content text NOT NULL,
@@ -44,10 +63,16 @@ db.query(
 		PRIMARY KEY (id),
 		FOREIGN KEY (quarter_id) REFERENCES quarters(id)
 	)
-`
-);
-db.query(
-    `
+`,
+        (err, res) => {
+            if (err) reject(err);
+            if (res) resolve(res);
+        }
+    );
+});
+let keyResults = new Promise((resolve, reject) => {
+    db.query(
+        `
 	CREATE TABLE IF NOT EXISTS key_results (
 		id INT(255) UNSIGNED NOT NULL AUTO_INCREMENT,
 		content text NOT NULL,
@@ -57,10 +82,16 @@ db.query(
 		PRIMARY KEY (id),
 		FOREIGN KEY (objective_id) REFERENCES objectives(id)
 	)
-`
-);
-db.query(
-    `
+`,
+        (err, res) => {
+            if (err) reject(err);
+            if (res) resolve(res);
+        }
+    );
+});
+let crfs = new Promise((resolve, reject) => {
+    db.query(
+        `
 	CREATE TABLE IF NOT EXISTS crfs (
 		id INT(255) UNSIGNED NOT NULL AUTO_INCREMENT,
 		date TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
@@ -70,5 +101,21 @@ db.query(
 		PRIMARY KEY (id),
 		FOREIGN KEY (key_result_id) REFERENCES key_results(id)
 	)
-`
-);
+`,
+        (err, res) => {
+            if (err) reject(err);
+            if (res) resolve(res);
+        }
+    );
+});
+
+Promise.all([users, bhags, quarters, objectives, keyResults, crfs])
+    .then((data) => {
+        console.log("Done");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+    .then(() => {
+        process.exit(0);
+    });
