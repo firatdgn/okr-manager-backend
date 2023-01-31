487875b150837bebae2d4f2d78042e94d5fce155
Fırat Doğan <fratdogan@yandex.com>
2023-02-01 00:54:46 +0300

    feat: token validation endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const jose = require("jose");
 
 router.get("/", function (req, res, next) {
     res.send("Hello World");
+});
+
+router.post("/is-token-valid", async (req, res) => {
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+    try {
+        const { payload, protectedHeader } = await jose.jwtVerify(
+            req.body["access-token"],
+            secret
+        );
+        if (payload) {
+            res.json({
+                status: "success",
+                message: "Token is valid.",
+            });
+        }
+    } catch (error) {
+        res.status(400).json({
+            status: "error",
+            message: "Token is not valid.",
+        });
+    }
 });
 
 module.exports = router;
